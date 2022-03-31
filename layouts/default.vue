43916278242b502164957dc64ed239a95a5869c6
ananducherakkal <ananducherakkal.dev@gmail.com>
2022-03-31 11:48:25 +0530

    Initial commit

---
+++ layouts/default.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <Nuxt />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'DefaultLayout'
+}
+</script>
+
+<style lang="scss">
+@import url('~/assets/scss/main.scss');
+</style>
